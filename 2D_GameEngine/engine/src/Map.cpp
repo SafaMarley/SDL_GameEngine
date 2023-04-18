48b115aef9348c4f40eb5b71d4f73a5eab14dd22
mSafa <safaktrcgl@gmail.com>
2023-04-19 00:34:01 +0300

    Carry lib dependencies inside the project update paths accordingly

--- 2D_GameEngine/engine/src/Map.cpp
+++ 2D_GameEngine/engine/src/Map.cpp
@@ -1,5 +1,5 @@
 #include "Map.h"
-#include "Game.h"
+#include "../../game/Game.h"
 #include <fstream>
 
 
--- 2D_GameEngine/game/Game.h
+++ 2D_GameEngine/game/Game.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "SDL.h"
-#include "SDL_image.h"
+#include "../libs/SDL_image.h"
 #include <iostream>
 #include <vector>
 
